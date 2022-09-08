251b9e3b070fa6f8bd9cf384c18064d6d328e69f
Fabian Rodriguez <fabianjra@hotmail.com>
2022-09-07 19:53:13 -0600

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by z419617 on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
